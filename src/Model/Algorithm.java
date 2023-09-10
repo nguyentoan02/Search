1cf25823c3a0981de0d0b76f0213e1573f203946
NguyenDucHoan170033 <144197148+NguyenDucHoan170033@users.noreply.github.com>
2023-09-10 18:49:48 +0700

    Update Algorithm.java

--- src/Model/Algorithm.java
+++ src/Model/Algorithm.java
@@ -21,6 +21,15 @@ public class Algorithm {
         }
     }
 
-    
+    public int linerSearch(int[] arr, int key) {
+
+        int size = arr.length;
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
 }
